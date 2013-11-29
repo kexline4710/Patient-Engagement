c96def45a73283976e8210aa965f52fbc6213a9f
kexline4710 <kathryn.exline@gmail.com>
2013-11-29 14:18:24 -0600

    adding all rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PatientEngagement::Application.config.secret_token = '03875fc13fed068c9e5bfdfce8ba71e1322ed468420a6a6f57d91c996d77a110b4123416381b7d988c94bfc54534e721518ddee0f6a517bfd446b36d26325961'
