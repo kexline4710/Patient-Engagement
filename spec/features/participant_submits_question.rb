77880e00adc109ee6443d78ea5684fadeed0aad3
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-30 15:16:47 -0600

    began creating question test

--- spec/features/participant_submits_question.rb
+++ spec/features/participant_submits_question.rb
@@ -1,1 +1,8 @@
-participant_submits_question.rb
+require 'spec_helper'
+
+describe "Participant Submits Questions" do
+
+  scenario "participant asks question with complete information" do
+    visit questions_path
+  end
+end
