787a50a54810d9e8f32b7fc1f8f6cc4ef7c45d4a
Clay Morton <lyonsclay@yahoo.com>
2013-11-30 09:55:41 -0600

    created coordinator model and tests with start of login test.

---
+++ app/models/coordinator.rb
@@ -0,0 +1,8 @@
+class Coordinator < ActiveRecord::Base
+  attr_accessible :email, :password_digest
+
+  validates :email, :presence => true
+  validates :email, :uniqueness => true
+  validates :password, :presence => true
+  has_secure_password
+end
