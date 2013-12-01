99a0fed50c67fdb7e080ad1ced1e0004828debe1
Clay Morton <lyonsclay@yahoo.com>
2013-11-30 23:39:24 -0600

    fixed coordinator_visits_dashboard test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,13 +7,9 @@ module ApplicationHelper
 	end
 
 	def current_user
-<<<<<<< HEAD
-	if Coordinator.find_by_authenticity_token(cookies[:authenticity_token]).nil?
-			@current_user ||= Participant.find_by_authenticity_token(cookies[:authenticity_token]) if cookies[:authenticity_token]
-=======
+
 		if Coordinator.find_by_authenticity_token(session[:authenticity_token]).nil?
 			@current_user ||= Participant.find_by_authenticity_token(session[:authenticity_token]) if session[:authenticity_token]
->>>>>>> f7e31e8c824a430f3a434c94a6fae2e43473bfbe
 		else
 			@current_user ||= Coordinator.find_by_authenticity_token(session[:authenticity_token]) if session[:authenticity_token]
 		end
