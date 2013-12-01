50bf68c77f331db3595d2d825785c5e60e4533bd
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2013-12-01 11:17:12 -0600

    created first_time_login_spec

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,11 +2,24 @@ FactoryGirl.define do
 	factory :participant do |f|
 		f.first_name "Clay"
 		f.last_name	"Morten"
-		f.email	"Cman1@aol.com"
+		f.email	"Cman5@aol.com"
 		f.password "password"
 		f.subject_number 123
 		f.coordinator_id 1
-		f.authenticity_token "abc123"
+		f.authenticity_token "12412wer"
 		f.first_time_login false
+	end
+end
+
+FactoryGirl.define do
+	factory :first_time do |f|
+		f.first_name "Clay"
+		f.last_name	"Morten"
+		f.email	"Cman5@aol.com"
+		f.password "password"
+		f.subject_number 123
+		f.coordinator_id 1
+		f.authenticity_token "12412wer"
+		f.first_time_login true
 	end
 end
