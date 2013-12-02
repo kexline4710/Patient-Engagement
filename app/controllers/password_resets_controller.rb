e81a4d6d753ab0c6d3d002062fd2dfc1258b1f28
dbc-apprentice <apprentice@devbootcamp.com>
2013-12-02 10:36:20 -0600

    fixed route to login_path after password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -16,7 +16,7 @@ class PasswordResetsController < ApplicationController
  	participant = current_user
  	if participant.update_attributes(password: params[:participant][:password_digest])
  		flash[:message] = ["Password Updated!"]
- 	redirect_to root_path
+ 	redirect_to login_path
  	else
  		flash[:message] = participant.errors.full_messages
  		redirect_to edit_password_reset_path(participant.authenticity_token)
