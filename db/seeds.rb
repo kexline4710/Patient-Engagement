9261d7b6ff1c88de276ffaa860151ae4eb6bf1ee
kexline4710 <kathryn.exline@gmail.com>
2013-12-01 18:55:51 -0600

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@ p1 = Participant.create(first_name: "Katy", last_name: "Exline", email: "kathryn
 p1.generate_password
 # p1.save
 # p1.send_initial_email
-p2 = Participant.create(first_name: "Jane", last_name: "Smith", email: "j@dbc.com", subject_number: 40009 )
+p2 = Participant.create(first_name: "Jane", last_name: "Smith", email: "j@dbc.com", password: "password", subject_number: 40009 )
 p2.generate_password
 # p2.save
-p3 = Participant.create(first_name: "Clay", last_name: "Lyons", email: "c@dbc.com", subject_number: 40010 )
+p3 = Participant.create(first_name: "Clay", last_name: "Lyons", email: "c@dbc.com", password: "password", subject_number: 40010 )
 p3.generate_password
 # p3.save
 
