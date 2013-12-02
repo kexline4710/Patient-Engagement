483d5cc851eee495051327349ad78b5c64a3034d
dbc-apprentice <apprentice@devbootcamp.com>
2013-12-02 10:33:42 -0600

    fixed routes

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,11 @@
+class DashboardsController < ApplicationController
+
+
+  def show
+    if current_user.class == Participant
+      redirect_to participant_path(current_user.authenticity_token)
+    else
+      redirect_to coordinator_path(current_user.authenticity_token)
+    end
+  end
+end
