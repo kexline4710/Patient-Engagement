f8581b915eb92285b424a061ed31bff2f193744c
kexline4710 <kathryn.exline@gmail.com>
2013-12-02 17:56:54 -0600

    added s

--- db/migrate/20131202035134_add_details_to_trials.rb
+++ db/migrate/20131202035134_add_details_to_trials.rb
@@ -1,4 +1,4 @@
-class AddDetailsToTrial < ActiveRecord::Migration
+class AddDetailsToTrials < ActiveRecord::Migration
   def change
     add_column :trials, :title, :string
     add_column :trials, :number, :integer
