60ccdd44cb3c3974ea0623d1946cd9658b23b73d
Clay Morton <lyonsclay@yahoo.com>
2013-12-01 21:06:32 -0600

    created route for trial#show and added test for trial model

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ PatientEngagement::Application.routes.draw do
   post 'login' => 'sessions#create'
   post 'logout' => 'sessions#destroy'
 
-
+  resources :trials, only: [:show]
 
   resources :participants
 
