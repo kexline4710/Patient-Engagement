55ac63d7d8bb25b38192454a8b8bb2967c8f6046
Clay Morton <lyonsclay@yahoo.com>
2013-12-02 19:30:19 -0600

    added associations to question_spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe Question do
   describe "associations" do
-    it { should belong_to(:participant)}
+    it { should belong_to(:participant) }
+    it { should have_one(:answer) }
+    it { should have_one(:coordinator) }
   end
 end
