8df33f1767394d32af029d7fb1554217b62086ba
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2013-12-03 17:06:01 -0600

    fixed error with content

---
+++ app/controllers/participant_files_controller.rb
@@ -0,0 +1,27 @@
+class ParticipantFilesController < ApplicationController
+  include ApplicationHelper
+  	include JsonHelper
+
+
+	def new
+		# File.open(File.join(Rails.root, 'lib', 'assets', '3.json')) do |f|
+		# 	f.each_line do |line|
+		# 		this_line = JSON.parse(line)
+		# 		keyset= "participant_1"
+		# 		remainders = this_line.slice!(*keyset)
+		# 		new_participant(this_line)
+		# 	end
+		# end	
+	end
+
+	def create
+		file = params[:che]
+		debugger
+		 json = File.read('file')
+		 obj= JSON.parse(json)
+
+		 puts obj
+	end
+
+
+end
