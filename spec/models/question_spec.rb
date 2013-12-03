a97b864bc758b9c4ac49c1eb110fb6936643b9e2
Clay Morton <lyonsclay@yahoo.com>
2013-12-02 20:19:37 -0600

    added test to question_spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,4 +6,15 @@ describe Question do
     it { should have_one(:answer) }
     it { should have_one(:coordinator) }
   end
+
+  describe "coordinator_name" do
+    let(:question) {FactoryGirl.create(:question) }
+    let(:answer) { FactoryGirl.create(:answer) }
+    let(:coordinator) {FactoryGirl.create(:coordinator)}
+
+    it "should equal coordinator.name" do
+      answer.update_attributes(question_id: question.id, coordinator_id: coordinator.id)
+      expect(question.coordinator_name).to eq(coordinator.first_name)
+    end
+  end
 end
