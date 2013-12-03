0960e4f702af231103d12443d80170efca9d1d2c
Clay Morton <lyonsclay@yahoo.com>
2013-12-03 14:22:14 -0600

    refactor participant_checks_answer test

--- spec/features/participant_checks_answer_status_spec.rb
+++ spec/features/participant_checks_answer_status_spec.rb
@@ -5,13 +5,11 @@ describe "the participant dashboard" do
   subject { page }
 
   describe "the participant checks status of pending questions" do
-
-    describe "there is one answered question" do
-       let(:participant) { FactoryGirl.create(:participant) }
+      let(:participant) { FactoryGirl.create(:participant) }
       let(:question) { FactoryGirl.create(:question)}
       let(:answer) { FactoryGirl.create(:answer) }
       let(:coordinator) { FactoryGirl.create(:coordinator) }
-      before do
+      before(:each) do
         answer.coordinator_id = coordinator.id
         answer.question = question
         answer.save
@@ -20,40 +18,19 @@ describe "the participant dashboard" do
         fill_in "Password", with: participant.password
         click_button "Login"
       end
+    describe "there is one answered question" do
       it { should have_content("You have answered questions!") }
     end
 
     describe "the participant views question" do
-      let(:participant) { FactoryGirl.create(:participant) }
-      let(:question) { FactoryGirl.create(:question)}
-      let(:answer) { FactoryGirl.create(:answer) }
-      let(:coordinator) { FactoryGirl.create(:coordinator) }
       before do
-        answer.coordinator_id = coordinator.id
-        answer.question = question
-        answer.save
-        visit root_path
-        fill_in "Email",    with: participant.email
-        fill_in "Password", with: participant.password
-        click_button "Login"
         find(:link, "You have answered questions!").click
       end
       it { should have_content(question.content)}
     end
 
     describe "the participant views question" do
-      let(:participant) { FactoryGirl.create(:participant) }
-      let(:question) { FactoryGirl.create(:question)}
-      let(:answer) { FactoryGirl.create(:answer) }
-      let(:coordinator) { FactoryGirl.create(:coordinator) }
       before do
-        answer.coordinator_id = coordinator.id
-        answer.question = question
-        answer.save
-        visit root_path
-        fill_in "Email",    with: participant.email
-        fill_in "Password", with: participant.password
-        click_button "Login"
         find(:link, "You have answered questions!").click
         find(:xpath, "//a[@href='/']").click
       end
