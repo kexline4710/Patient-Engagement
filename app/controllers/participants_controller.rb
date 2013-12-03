f94dc84c02fae4ca9c7ae74ed287933c38d5934d
kexline4710 <kathryn.exline@gmail.com>
2013-12-03 13:49:14 -0600

    feelings now displayed in the patient dashboard

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -10,12 +10,12 @@ class ParticipantsController < ApplicationController
   def show
     @questions = five_recent_questions
     @feelings = Feeling.all
+    @current_feeling = current_user.feeling.emotion
   end
 
   def update
-  	id = params[:feeling_id]
-  	p id
-  	current_user.update_attribute(feeling_id: params[:feeling_id])
+  	current_user.update_attribute(:feeling_id, params[:feeling_id])
+    redirect_to participant_path(current_user)
   end
 
 end
