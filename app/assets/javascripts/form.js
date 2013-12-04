bce79bc5cf78d9fa2e1ac39b04d18bab972b43e2
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2013-12-04 08:56:08 -0600

    modal working

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -2,12 +2,7 @@ $(document).ready(function(){
 	$(".answer_button").on('submit', function(event){
 			event.preventDefault();
 	$.get("/answers/new?utf=✓&question=2&commit=Answer.html", function(data){
-			var page = $(data);
-			var contents = page.filter('#id');
-			console.log(contents);
-			debugger
-
-			$(contents.prevObject).dialog();
+			$(data).find("form").dialog({title: "Answer Question", modal: true});
 	});
 
 	});
