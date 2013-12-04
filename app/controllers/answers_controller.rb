7380dbc18cb00676528c81715bddf4f02d2fffcd
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2013-12-03 21:18:55 -0600

    fixed alert functions and search and other things

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -11,6 +11,8 @@ class AnswersController < ApplicationController
     content = params[:answer][:content]
     question = Question.find(params[:question_id].to_i)
     answer = Answer.create(content: content, question_id: question.id, coordinator_id: current_user.id)
+    question.answered = true
+    question.save
     flash[:message] = ["#{question.title} answered and added to archive"]
     # current_user.coordinator.send_quest
     redirect_to coordinator_path(current_user)
