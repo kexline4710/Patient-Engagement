c41b44f27fcfd1c6d5d1a614444d99ab6f7fe055
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2013-12-04 09:20:07 -0600

    fully working modal

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 	$(".answer_button").on('submit', function(event){
 			event.preventDefault();
-	$.get("/answers/new?utf=✓&question=2&commit=Answer.html", function(data){
-			$(data).find("form").dialog({title: "Answer Question", modal: true});
-	});
+			console.log(event)
+			debugger
+	// $.get("/answers/new?utf=✓&question=2&commit=Answer.html", function(data){
+	// 		$(data).find("form").dialog({title: "Answer Question", modal: true, height: "auto", width: "auto"});
+	// });
 
 	});
 
