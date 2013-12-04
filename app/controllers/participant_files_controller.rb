0ec4f4542148bc945a85f422991ff20eec9b2050
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2013-12-03 18:06:25 -0600

    Finished Basic Json Uploader

--- app/controllers/participant_files_controller.rb
+++ app/controllers/participant_files_controller.rb
@@ -4,25 +4,18 @@ class ParticipantFilesController < ApplicationController
 
 
 	def new
-		# File.open(File.join(Rails.root, 'lib', 'assets', '3.json')) do |f|
-		# 	f.each_line do |line|
-		# 		this_line = JSON.parse(line)
-		# 		keyset= "participant_1"
-		# 		remainders = this_line.slice!(*keyset)
-		# 		new_participant(this_line)
-		# 	end
-		# end	
 	end
 
 	def create
-		subject_list = params[:participant_file][:subject_list]
-		 json_list = subject_list.read
-		 parsed_list = JSON.parse(json_list)
-		 parsed_list.each do |key, value| 
-		 		new_participant(value)
-		 end
-		 flash[:message] = ["#{parsed_list.length} Participants Uploaded Successfully"]
+	 parse_subject_list(params[:participant_file][:subject_list],params[:participant_file][:current_user])
+			
+		if true
+		 flash[:message] = ["Participants Uploaded Successfully"]
 		 redirect_to participant_files_path
+		else
+			flash[:message] = ["Participants were not loaded successfully, please try again"]
+			redirect_to new_participant_files_path
+		end
 	end
 
 	def index
--- app/helpers/json_helper.rb
+++ app/helpers/json_helper.rb
@@ -1,13 +1,23 @@
 module JsonHelper 
 
-	def new_participant(line)
+	def parse_subject_list(subject_list,coordinator_id)
+
+		json_list = subject_list.read
+		 parsed_list = JSON.parse(json_list)
+		 parsed_list.each do |key, value| 
+		 		new_participant(value, coordinator_id)
+		 end
+	end
+
+	def new_participant(subject,coordinator)
 		p=Participant.new
-		p.first_name     = line[0]
-		p.last_name	     = line[1]
-		p.email			     = line[2]
-		p.phone_number	 = line[3]
-		p.password       = line[4]
-		p.subject_number = line[5]
+		p.first_name     = subject[0]
+		p.last_name	     = subject[1]
+		p.email			     = subject[2]
+		p.phone_number	 = subject[3]
+		p.password       = subject[4]
+		p.subject_number = subject[5]
+		p.coordinator_id = coordinator.to_i
 		p.save!
 	end
 end
