b358da3328b5daa2b4bcea1eac3257ae2be59b0b
Clay Morton <lyonsclay@yahoo.com>
2013-12-04 10:34:31 -0600

    fixed password confirmation on password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,25 +1,31 @@
 class PasswordResetsController < ApplicationController
   skip_before_filter :require_login
 
- def new
- end
+  def new
+  end
 
- def create
- end
+  def create
+  end
 
- def edit
- 	@participant = current_user
- end
+  def edit
+  	@participant = current_user
+  end
 
- def update
- 	participant = current_user
- 	if participant.update_attributes(password: params[:participant][:password_digest])
- 		flash[:message] = ["Password Updated!"]
- 	redirect_to login_path
- 	else
- 		flash[:message] = participant.errors.full_messages
- 		redirect_to edit_password_reset_path(participant.authenticity_token)
- 	end
- end
-
+  def update
+   	participant = current_user
+    if params[:participant][:password_digest].length > 6
+      if params[:participant][:password_digest] == params[:participant][:password_confirmation]
+       	if participant.update_attributes(password: params[:participant][:password_digest])
+       		flash[:message] = ["Password Updated!"]
+       	redirect_to login_path
+        end
+   	  else
+        flash[:message] = ["Password and password confirmation must match!"]
+        redirect_to edit_password_reset_path(participant.authenticity_token)
+      end
+    else
+      flash[:message] = ["Password must be at least 6 characters!"]
+      redirect_to edit_password_reset_path(participant.authenticity_token)
+    end
+  end
 end
