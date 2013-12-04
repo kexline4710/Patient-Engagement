a83b8368eac6e42c012f54c24115b91ea57c7d83
Clay Morton <lyonsclay@yahoo.com>
2013-12-03 23:26:02 -0600

    uncommented first_time_login test

--- spec/features/first_time_login_spec.rb
+++ spec/features/first_time_login_spec.rb
@@ -3,28 +3,28 @@ require 'spec_helper'
 describe "First Time User Reset" do
 	subject { page }
 
-	# describe "login page" do
- #    before { visit root_path }
- #    it { should have_content('Email') }
- #  end
+	describe "login page" do
+    before { visit root_path }
+    it { should have_content('Email') }
+  end
 
- #  describe "allows first time user to login" do
- #  	  participant  = FactoryGirl.create(:participant, first_time_login: true)
+  describe "allows first time user to login" do
+  	  participant  = FactoryGirl.create(:participant, first_time_login: true)
 
- #  		before do
- #  		visit root_path
- #      fill_in "Email",    with: participant.email
- #      fill_in "Password", with: participant.password
- #      click_button "Login"
- #    	end
- #    	it { should have_content('Reset Password') }
- #  			it "allows participant to reset password" do
-	#   			fill_in "Password", with: "Alexander"
-	#   			fill_in "Password_Confirmation", with: "Alexander"
-	#   			click_button "Update Password"
-	# 			expect(page).to have_content("Email")
-	#   		end
-	#  end
+  		before do
+  		visit root_path
+      fill_in "Email",    with: participant.email
+      fill_in "Password", with: participant.password
+      click_button "Login"
+    	end
+    	it { should have_content('Reset Password') }
+  			it "allows participant to reset password" do
+	  			fill_in "Password", with: "Alexander"
+	  			fill_in "Password_Confirmation", with: "Alexander"
+	  			click_button "Update Password"
+				expect(page).to have_content("Email")
+	  		end
+	 end
 end
 
 
