a485f880235c8bb616e1b256154fd8792dc868a9
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2013-12-04 10:08:31 -0600

    working on outerHTML

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
 	$(".answer_button").on('submit', function(event){
 			event.preventDefault();
-			console.log(event)
-			debugger
-	// $.get("/answers/new?utf=✓&question=2&commit=Answer.html", function(data){
-	// 		$(data).find("form").dialog({title: "Answer Question", modal: true, height: "auto", width: "auto"});
-	// });
+		var num = $(event.target.outerHTML).find('input[name=question]').val()
+	$.get("/answers/new?utf=✓&question='+num+'&commit=Answer.html", function(data){
+			$(data).find("form").dialog({title: "Answer Question", modal: true, height: "auto", width: "auto"});
+	});
 
 	});
 
