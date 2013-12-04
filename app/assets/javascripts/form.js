7f01c6edf5a881bd258669955946ae324a77e286
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2013-12-04 10:34:40 -0600

    Working jquery for questions

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
 	$(".answer_button").on('submit', function(event){
 			event.preventDefault();
-		var num = $(event.target.outerHTML).find('input[name=question]').val()
+		var num = $(event.target.outerHTML).find('input[name=question]').val();
 	$.get("/answers/new?utf=✓&question=" + num + "&commit=Answer.html", function(data){
 			$(data).find("form").dialog({title: "Answer Question", modal: true, height: "auto", width: "auto"});
 		});
 	});
+
+	$("#ask_question").on('click', function(event){
+			event.preventDefault();
+			var link = $(event.currentTarget).attr("href")
+	$.get(link, function(data){
+			$(data).find("form").dialog({title: "Ask a Question", modal: true, height: "auto", width: "auto"});
+		});
+	});
+
 });
