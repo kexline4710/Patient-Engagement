d2178f70b501210ad8b4fcd5f651ccbb4f455349
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2013-12-04 10:13:37 -0600

    jquery working with answers

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -2,13 +2,8 @@ $(document).ready(function(){
 	$(".answer_button").on('submit', function(event){
 			event.preventDefault();
 		var num = $(event.target.outerHTML).find('input[name=question]').val()
-	$.get("/answers/new?utf=✓&question='+num+'&commit=Answer.html", function(data){
+	$.get("/answers/new?utf=✓&question=" + num + "&commit=Answer.html", function(data){
 			$(data).find("form").dialog({title: "Answer Question", modal: true, height: "auto", width: "auto"});
+		});
 	});
-
-	});
-
-
-
-
 });
