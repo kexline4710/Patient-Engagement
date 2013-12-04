08ff31a0bfea355d70c75273aecc65e559045de0
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2013-12-04 17:58:09 -0600

    fixed so button is not clickable

--- app/assets/javascripts/answered_questions.js
+++ app/assets/javascripts/answered_questions.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
-	$("#answered_unviewed").on('click', function(event){
+	$(".answered_unviewed").on('click', function(event){
 		event.preventDefault();
 		var link = $(event.currentTarget).attr("href")
 			$.get(link, function(data){
 				var answers = $(data).find('.col-md-8')
 				var slider = ('#slide_info')
 				$(slider).html(answers).hide()
-				console.log("here")
 				$(slider).slideDown("slow", function(){
-					console.log("and here")
 					$(slider).show();
-					$(document).on('click', function(){
-						$(slider).hide();
-						$("#answered_unviewed").hide();
+					$(".answered_unviewed").hide(1000, "easeInCirc");
+					$(document).on('click', function(e){
+						$(slider).slideUp();
+						
+							
 					});
 				
 				});	
