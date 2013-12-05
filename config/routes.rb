373a4b2cb3321bedba050b7f9e777d9a96e0233c
dbc-apprentice <apprentice@devbootcamp.com>
2013-12-05 15:51:37 -0600

    Changed logout path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ PatientEngagement::Application.routes.draw do
 
   get  'login'=> "sessions#new"
   post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
   post 'logout' => 'sessions#destroy'
+
 
   resources :trials, only: [:show]
 
