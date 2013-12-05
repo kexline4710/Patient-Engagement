0528e364f989f27c9c770a71780d2ffc37151d75
Jane <spiralm63@gmail.com>
2013-12-04 21:18:55 -0600

    footer started with javascript

---
+++ app/assets/javascripts/footer.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var height_diff = $(window).height() - $('body').height();
+    if ( height_diff > 0 ) {
+        $('#footer').css( 'margin-top', height_diff + 50 );
+    }
+});
+
