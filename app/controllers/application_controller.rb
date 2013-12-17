d8bbd79929e10c61002024ccbbdb806dffb14097
Clay Morton <lyonsclay@yahoo.com>
2013-12-16 18:24:10 -0600

    comments for refactor covered app/conrollers - models not views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,17 @@ class ApplicationController < ActionController::Base
   before_filter :require_login
   include ApplicationHelper
 
-
+## Bug - if first_time_login is triggered and user doesn't reset password,
+## but instead log's out the next time they login they can login with the
+## sent password. Also there is no check if the new password is different
+## than the old. -clm
   def first_time_visiting?
   	if current_user.try(:first_time_login)
   		current_user.update_attributes(:first_time_login => false)
       current_user.save(validate: false)
+
+## redirect_to edit_password_reset_path(current_user) and return
+## There is no need to specify authenticity_token for path.  -clm
   		redirect_to edit_password_reset_path(current_user.authenticity_token) and return
   	else
      redirect_to participant_path(current_user.authenticity_token) and return
